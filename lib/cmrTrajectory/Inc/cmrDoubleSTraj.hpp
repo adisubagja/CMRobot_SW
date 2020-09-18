417e3839f0955a82c5adfef0780d07738a67472b
NiFeijian <feijian.ni@flexiv.com>
2020-09-18 17:24:49 +0800

    tmp save

---
+++ lib/cmrTrajectory/Inc/cmrDoubleSTraj.hpp
@@ -0,0 +1,70 @@
+/*
+ *cmrDoubleSTraj.hpp
+ *
+ * this file generate trapzoidal trajectory in joint space
+ *
+ * Author: Feijian.ni
+ * Date: 2020.7.4
+ */
+
+#ifndef _CMRTRAPZOIDALTRAJ_HPP_
+#define _CMRTRAPZOIDALTRAJ_HPP_
+
+#include "cmrMatrix.hpp"
+
+namespace cmr {
+
+//------------------Double S trajectory class----------------------------
+class cmrDoubleSTraj {
+public:
+  cmrDoubleSTraj();
+  ~cmrDoubleSTraj();
+
+  cmrDoubleSTraj(const cmrDoubleSTraj&) = delete;
+  cmrDoubleSTraj& operator=(const cmrDoubleSTraj&) =delete; 
+  //! generate velocity for waypoints
+
+private:
+  //! flag if trajectory is generated successfully
+  bool m_trajGenerated;
+
+  //! acceleration duration
+  double m_accDuration;
+
+  //! deceleration duration
+  double m_decDuration;
+
+  //! constant velocity duration;
+  double m_constDuration;
+
+  //! trajectory max velocity
+  double m_maxTrajVel;
+
+  //！ trajectory max acceleration
+  double m_maxTrajAcc;
+};
+
+//------------------single dimension basic Double S class----------------------------
+class cmrBasicDoubleS{
+public:
+  cmrBasicDoubleS();
+  ~cmrBasicDoubleS();
+
+  //! init
+  void init(double q0, double q1, double v0, double v1, double maxV, double maxA, double maxJ);
+
+private:
+  //! limit acceleration 
+  double m_limitAcc;
+
+  //! limit deceleration
+  double m_limitDec;
+
+  //! limit velocity
+  double m_limitVel;
+
+  //!
+};
+
+} // namespace cmr
+#endif
